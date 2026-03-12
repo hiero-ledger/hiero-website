5ccc635e117f503db3a59319da754bf173fbf423
Daniel Ntege <danientege785@gmail.com>
2026-03-12 03:05:31 +0300

    chore: home page refactoering some errors
    Signed-off-by: Daniel Ntege <danientege785@gmail.com>
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;

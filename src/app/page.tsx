128b5c7bd7c4ad5e6ebfa8833982c25e01a6d359
vidhhya1 <vidya2005varshini@gmail.com>
2026-04-05 02:14:21 +0530

    fix: move 'Jump to Open Issues' section above Community Calls
    Signed-off-by: vidhhya1 <vidya2005varshini@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -378,13 +378,14 @@ export default function Home() {
     <>
       <HeroSection data={heroData} />
       <WhatIsHieroSection data={whatIsHieroData} />
+      <Divider /> 
+      <IssueJumpSection data={issueJumpData} />
       <Divider />
       <MeetSection data={meetData} />
       <Divider />
       <ReposCarousel data={reposData} />
       <Divider />
-      <IssueJumpSection data={issueJumpData} />
-      <Divider />
+     
       <OpenSourceSection data={openSourceData} />
       <Divider />
       <QuotesCarousel data={quotesData} />

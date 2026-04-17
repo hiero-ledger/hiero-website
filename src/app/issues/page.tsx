c03f92a90ad557b25f4d6641c33e126555d4ec85
aceppaluni <aceppaluni@gmail.com>
2026-04-17 14:37:34 -0400

    fix: Update new fixes
    Signed-off-by: aceppaluni <aceppaluni@gmail.com>

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -14,12 +14,12 @@ interface GitHubSearchResponse {
   items: GitHubIssue[];
 }
 
-function hasKey<T extends object>(
+/*function hasKey<T extends object>(
   obj: T,
   key: PropertyKey
 ): key is keyof T {
   return key in obj;
-}
+}*/
 
 export default function GoodFirstIssues() {
   const [issues, setIssues] = useState<GitHubIssue[]>([]);

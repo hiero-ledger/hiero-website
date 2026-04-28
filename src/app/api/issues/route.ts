cb8c6f013bde8369526cc54e78139bc4e8620af9
aceppaluni <aceppaluni@gmail.com>
2026-04-28 13:41:04 -0400

    fix: Fix Updated Errs
    Signed-off-by: aceppaluni <aceppaluni@gmail.com>

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,28 +1,4 @@
-import { searchIssues as searchIssues } from "src/lib/github/issues";
-
-/*interface GitHubIssue {
-  id: number;
-  title: string;
-  html_url: string;
-  repository_url: string;
-}
-
-interface GitHubSearchResponse {
-  items: GitHubIssue[];
-  error?: string;
-}*/
-
-//type SearchIssuesFn = (_query: string) => Promise<GitHubSearchResponse>;
-//const searchIssues = rawSearchIssues as SearchIssuesFn;
-
-/*export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const q = searchParams.get("q") ?? "";
-
-  const data = await searchIssues(q);
-
-  return Response.json(data);
-}*/
+import { searchIssues } from "../../../lib/github/issues";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);

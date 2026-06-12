a616ca3def62b543d0d18d235899cc7188a0ae6b
aceppaluni <aceppaluni@gmail.com>
2026-06-12 16:03:01 -0400

    fix: Additional Requires
    Signed-off-by: aceppaluni <aceppaluni@gmail.com>

--- .github/scripts/helpers/index.js
+++ .github/scripts/helpers/index.js
@@ -5,12 +5,12 @@
 // Single entry point for bot helpers. Re-exports constants, logger, validation,
 // API, checks, and comments.
 
-const constants = require('./constants');
-const logger = require('./logger');
-const validation = require('./validation');
-const api = require('./api');
-const checks = require('./checks');
-const comments = require('./comments');
+import * as constants from './constants.js';
+import * as logger from './logger.js';
+import * as validation from './validation.js';
+import * as api from './api.js';
+import * as checks from './checks.js';
+import * as comments from './comments.js';
 
 export default {
   ...constants,
